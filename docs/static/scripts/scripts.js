f2a99aba7db668f869d0abf89bcfc80a326078a1
Max Gorman <89482087+Djlispy@users.noreply.github.com>
2025-04-14 19:08:42 -0400

    test dark mode

--- docs/static/scripts/scripts.js
+++ docs/static/scripts/scripts.js
@@ -3,7 +3,9 @@ var map = L.map('map');
 map.setView([28.07571502097867, -82.60894808034853], 9);
 
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-    maxZoom: 19,
+    minZoom: 2,
+    maxZoom: 20,
+    ext: 'png',
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
