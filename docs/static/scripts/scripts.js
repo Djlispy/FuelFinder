00fcbb800076391fe6b70022104103c1b5a97d9d
Max Gorman <89482087+Djlispy@users.noreply.github.com>
2025-04-14 19:55:02 -0400

    major changes

--- docs/static/scripts/scripts.js
+++ docs/static/scripts/scripts.js
@@ -3,14 +3,7 @@ var map = L.map('map');
 map.setView([28.07571502097867, -82.60894808034853], 9);
 
 var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	// attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	minZoom: 8,
+	maxZoom: 10,
 	ext: 'png'
 }).addTo(map)
-
-// var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-//     minZoom: 2,
-//     maxZoom: 20,
-//     ext: 'png'
-// }).addTo(map)
