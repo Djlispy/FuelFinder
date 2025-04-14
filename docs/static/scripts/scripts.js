6ef41c321ddf6b19a35ab478dc4ad5716ce3a694
Max Gorman <89482087+Djlispy@users.noreply.github.com>
2025-04-14 19:04:01 -0400

    test map

--- docs/static/scripts/scripts.js
+++ docs/static/scripts/scripts.js
@@ -2,8 +2,7 @@
 var map = L.map('map');
 map.setView([28.07571502097867, -82.60894808034853], 9);
 
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 2,
-    maxZoom: 20,
-    ext: 'png'
-}).addTo(map)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
