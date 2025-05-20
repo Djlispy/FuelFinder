9f6433539dd8738f47d726ed335701377d3ced3f
Max Gorman <89482087+Djlispy@users.noreply.github.com>
2025-05-19 23:50:33 -0400

    update zoom

--- docs/static/scripts/scripts.js
+++ docs/static/scripts/scripts.js
@@ -3,8 +3,8 @@ var map = L.map('map');
 map.setView([28.07571502097867, -82.60894808034853], 9);
 
 var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 8,
-	maxZoom: 10,
+	minZoom: 5,
+	maxZoom: 14,
 	ext: 'png'
 }).addTo(map)
 
