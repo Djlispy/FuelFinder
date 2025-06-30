24d64dd9d4e86de2b564904e7b7ce1e26181cd3a
Max Gorman <djlispy@gmail.com>
2025-06-29 23:12:52 -0400

    debug

--- docs/get_token.py
+++ docs/get_token.py
@@ -2,37 +2,38 @@ from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium_stealth import stealth
+import time
+
 
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("window-size=1920,1080")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
 
 
 driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options)
-driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-  "source": """
-    Object.defineProperty(navigator, 'webdriver', {get: () => undefined})
-  """
-})
 
-# Stealth to reduce detection
 stealth(driver,
     languages=["en-US", "en"],
     vendor="Google Inc.",
-    platform="Win32",  # Sometimes Windows is less suspicious than MacIntel on CI
+    platform="Win32",  # sometimes Windows is less suspicious than MacIntel on Linux runners
     webgl_vendor="Intel Inc.",
     renderer="Intel Iris OpenGL Engine",
     fix_hairline=True,
 )
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+# Also execute JS to overwrite webdriver property:
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
 
 
 # Visit the site
 driver.get("https://www.7-eleven.com/locator")
-
+time.sleep(2)
 print(driver.title)
 print(driver.page_source[:4000])  
 # for req in driver.requests:
